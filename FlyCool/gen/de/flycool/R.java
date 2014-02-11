cc487cc961d35effe15b00e7870815681e020183
daniel.hardt <d.hardt96@t-online.de>
2014-02-11 12:50:37 +0100

    Resources

--- FlyCool/gen/de/flycool/R.java
+++ FlyCool/gen/de/flycool/R.java
@@ -17,25 +17,66 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_settings=0x7f090016;
+        public static final int altitudeGndLabel=0x7f09000c;
+        public static final int altitudeGndValue=0x7f09000d;
+        public static final int altitudeMslLabel=0x7f090012;
+        public static final int altitudeMslValue=0x7f090013;
+        public static final int gndLabel=0x7f090007;
+        public static final int latitudeLabel=0x7f090002;
+        public static final int latitudeValue=0x7f090003;
+        public static final int locationLabel=0x7f090001;
+        public static final int longitudeLabel=0x7f090004;
+        public static final int longitudeValue=0x7f090005;
+        public static final int maxAltitudeMslLabel=0x7f090010;
+        public static final int maxAltitudeMslValue=0x7f090011;
+        public static final int minAltitudeGndLabel=0x7f09000a;
+        public static final int minAltitudeGndValue=0x7f09000b;
+        public static final int mslAltitudeGndLabel=0x7f090008;
+        public static final int mslAltitudeGndValue=0x7f090009;
+        public static final int mslLabel=0x7f09000f;
+        public static final int relativeLayoutGnd=0x7f090006;
+        public static final int relativeLayoutMsl=0x7f09000e;
+        public static final int relativeLayoutPosition=0x7f090000;
+        public static final int warnFrame=0x7f090014;
+        public static final int warnMessage=0x7f090015;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
     }
     public static final class menu {
-        public static final int main=0x7f070000;
+        public static final int main=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
+        public static final int action_settings=0x7f060001;
+        public static final int altitudeGndLabel=0x7f06000d;
+        public static final int altitudeMslLabel=0x7f060010;
+        public static final int app_name=0x7f060000;
+        public static final int climb=0x7f060012;
+        public static final int gndLabel=0x7f06000a;
+        public static final int latitudeLabel=0x7f060008;
+        public static final int locationLabel=0x7f060007;
+        public static final int longitudeLabel=0x7f060009;
+        public static final int maxAltitudeMslLabel=0x7f06000f;
+        public static final int minAltitudeGndLabel=0x7f06000c;
+        public static final int mslAltitudeGndLabel=0x7f06000b;
+        public static final int mslLabel=0x7f06000e;
+        public static final int no_gps_button=0x7f060015;
+        public static final int no_gps_message=0x7f060014;
+        public static final int no_gps_title=0x7f060013;
+        public static final int pref_summary_max_altitude_msl=0x7f060006;
+        public static final int pref_summary_min_altitude_gnd=0x7f060004;
+        public static final int pref_title_altitudes=0x7f060002;
+        public static final int pref_title_max_altitude_msl=0x7f060005;
+        public static final int pref_title_min_altitude_gnd=0x7f060003;
+        public static final int sink=0x7f060011;
     }
     public static final class style {
         /** 
@@ -59,10 +100,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+    }
+    public static final class xml {
+        public static final int preferences=0x7f040000;
     }
 }
